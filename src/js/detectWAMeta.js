db752b1090d9284b84bf067384fdfbbaef67a679
Richard Hansen <rrh@fb.com>
2021-08-12 12:34:42 -0700

    Add initial support for WA
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ src/js/detectWAMeta.js
@@ -0,0 +1,14 @@
+(
+() => {
+    // extract JS version from the page
+    // setup listener to listen for Service Worker to tell us it has the manifest
+    // send message to Service Worker to download the correct manifest
+    console.log('sending message to worker');
+    chrome.runtime.sendMessage({type: "LOAD_MANIFEST", origin:"WHATSAPP", version: 1}, (response) => {
+        console.log('response is ', response);
+        scanForScripts();
+    });
+    console.log('after message');
+    // call scanForScripts to scan the page for JS, then pipe it to Service Worker to hash and compare?
+}
+)();
