6c72db34e0a56de85e438352701f8ac4f76d1f2c
Richard Hansen <rrh@fb.com>
2021-08-31 15:31:19 -0700

    add todo tests for contentUtils.

---
+++ src/js/__tests__/contentUtils-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('contentUtils', () => {
+    test.todo('test for storeFoundJS');
+    test.todo('test for hasInvalidAttributes');
+    test.todo('test for hasInvalidScripts');
+    test.todo('test for scanForScripts');
+    test.todo('test for processFoundJS');
+    test.todo('ensure processing icon message is sent');
+});
