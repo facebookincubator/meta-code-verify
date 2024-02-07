9cb16beef44bfa45d4bd203bbdbff1bb3b4f015d
Ezzudin Alkotob <ezz@fb.com>
2024-02-06 18:14:26 -0800

    simplify build

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type { Plugin, RollupOptions } from 'rollup';
+import type {Plugin, RollupOptions} from 'rollup';
 
 import cleanOnce from './build/rollup-plugin-clean-once';
 import eslintPlugin from '@rollup/plugin-eslint';
@@ -15,107 +15,58 @@ import staticFiles from './build/rollup-plugin-static-files';
 import watch from './build/rollup-plugin-watch-additional';
 
 function eslint(): Plugin {
-    return eslintPlugin({throwOnError: true});
+  return eslintPlugin({throwOnError: true});
 }
 function prettierSrc(): Plugin {
-    return prettierBuildStart('"src/**/*.(js|ts)"');
+  return prettierBuildStart('"src/**/*.(js|ts)"');
 }
 
-const config: Array<RollupOptions> = [
-    {
-        input: 'src/js/detectWAMeta.ts',
-        output: [{
-            file: 'dist/chrome/contentWA.js',
-            format: 'iife'
-        }, {
-            file: 'dist/edge/contentWA.js',
-            format: 'iife'
-        }, {
-            file: 'dist/firefox/contentWA.js',
-            format: 'iife'
-        }],
-        plugins: [cleanOnce(), typescript(), prettierSrc(), eslint()],
-    },
-    {
-        input: 'src/js/detectMSGRMeta.ts',
-        output: [{
-            file: 'dist/chrome/contentMSGR.js',
-            format: 'iife'
-        }, {
-            file: 'dist/edge/contentMSGR.js',
-            format: 'iife'
-        }, {
-            file: 'dist/firefox/contentMSGR.js',
-            format: 'iife'
-        }],
-        plugins: [typescript(), prettierSrc(), eslint()],
-    },
-    {
-        input: 'src/js/detectFBMeta.ts',
-        output: [{
-            file: 'dist/chrome/contentFB.js',
-            format: 'iife'
-        }, {
-            file: 'dist/edge/contentFB.js',
-            format: 'iife'
-        }, {
-            file: 'dist/firefox/contentFB.js',
-            format: 'iife'
-        }],
-        plugins: [typescript(), prettierSrc(), eslint()],
-    },
-    {
-        input: 'src/js/detectIGMeta.ts',
-        output: [{
-            file: 'dist/chrome/contentIG.js',
-            format: 'iife'
-        }, {
-            file: 'dist/edge/contentIG.js',
-            format: 'iife'
-        }, {
-            file: 'dist/firefox/contentIG.js',
-            format: 'iife'
-        }],
-        plugins: [typescript(), prettierSrc(), eslint()],
-    },
-    {
-        input: 'src/js/background.ts',
-        output: [{
-            file: 'dist/chrome/background.js',
-            format: 'iife'
-        }, {
-            file: 'dist/edge/background.js',
-            format: 'iife'
-        }, {
-            file: 'dist/firefox/background.js',
-            format: 'iife'
-        }],
-        plugins: [typescript(), prettierSrc(), eslint()],
-    },
-    {
-        input: 'src/js/popup.ts',
-        output: [{
-            file: 'dist/chrome/popup.js',
-            format: 'iife',
-            plugins: [staticFiles('config/v3/')],
-        }, {
-            file: 'dist/edge/popup.js',
-            format: 'iife',
-            plugins: [staticFiles('config/v3/')],
-        }, {
-            file: 'dist/firefox/popup.js',
-            format: 'iife',
-            plugins: [staticFiles('config/v2/')],
-        }],
-        plugins: [
-            typescript(),
-            prettierSrc(),
-            eslint(),
-            staticFiles(['images/', 'src/css/', 'src/html/']),
-            staticFiles('_locales/', {keepDir: true}),
-            watch(['images/', 'src/css/', 'src/html/', '_locales/', 'config/']),
-        ],
-    }
-];
+const TARGETS_TO_MANIFEST_VERSION = {chrome: 'v3', edge: 'v3', firefox: 'v2'};
+const SITES = ['WA', 'MSGR', 'FB', 'IG'];
+
+const contentScriptSteps: Array<RollupOptions> = SITES.map((site, index) => ({
+  input: `src/js/detect${site}Meta.ts`,
+  output: Object.keys(TARGETS_TO_MANIFEST_VERSION).map(target => ({
+    file: `dist/${target}/content${site}.js`,
+    format: 'iife',
+  })),
+  plugins: [
+    index === 0 ? cleanOnce() : null,
+    typescript(),
+    prettierSrc(),
+    eslint(),
+  ],
+}));
+
+const config: Array<RollupOptions> = contentScriptSteps.concat([
+  {
+    input: 'src/js/background.ts',
+    output: Object.keys(TARGETS_TO_MANIFEST_VERSION).map(target => ({
+      file: `dist/${target}/background.js`,
+      format: 'iife',
+    })),
+    plugins: [typescript(), prettierSrc(), eslint()],
+  },
+  {
+    input: 'src/js/popup.ts',
+    output: (
+      Object.keys(TARGETS_TO_MANIFEST_VERSION) as Array<
+        keyof typeof TARGETS_TO_MANIFEST_VERSION
+      >
+    ).map(target => ({
+      file: `dist/${target}/popup.js`,
+      format: 'iife',
+      plugins: [staticFiles(`config/${TARGETS_TO_MANIFEST_VERSION[target]}/`)],
+    })),
+    plugins: [
+      typescript(),
+      prettierSrc(),
+      eslint(),
+      staticFiles(['images/', 'src/css/', 'src/html/']),
+      staticFiles('_locales/', {keepDir: true}),
+      watch(['images/', 'src/css/', 'src/html/', '_locales/', 'config/']),
+    ],
+  },
+]);
 
 export default config;
